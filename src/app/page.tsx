917dc6f9e05cb3d711394db2ef76ffd549932854
mohamed-hema <mohamedhema2017@gmail.com>
2025-02-13 19:57:18 +0200

    basic navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-primary">Hello World</h1>
+      <Button className="bg-blue-500">Hello</Button>
     </main>
   );
 }
